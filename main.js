603e548821e4ac35d1b3f4062ad7934f848c0367
manEthan <78244392+manEthan@users.noreply.github.com>
2021-05-02 21:41:43 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+document.querySelector('select').addEventListener('change', function() {
+  const c = this.value;
+  console.log("start...");
+  switch (c) {
+    case 'superman':
+      document.body.style.backgroundImage = "url('./images/superman.jpeg')";
+      document.body.style.backgroundSize = "cover";
+      document.body.style.backgroundPosition = "center";
+      break;
+
+    case 'ironman':
+      document.body.style.backgroundImage = "url('./images/ironman.jpeg')";
+      document.body.style.backgroundSize = "cover";
+      document.body.style.backgroundPosition = "center";
+      break;
+
+    default:
+      document.body.style.background = 'white';
+      break;
+  }
+})
+
 window.onload = () => {
   'use strict';
 
