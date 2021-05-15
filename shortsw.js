7809c6f535c96e09c569b061fd2568c8b1ef2968
manEthan <78244392+manEthan@users.noreply.github.com>
2021-05-15 13:40:26 -0700

    Add files via upload

---
+++ shortsw.js
@@ -0,0 +1,25 @@
+var cacheName = 'ethanshorttask';
+var filesToCache = [
+  '/',
+  '/shorttaskindex.html',
+  '/shorttaskstyle.css',
+  '/shortmain.js'
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
